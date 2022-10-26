38718ca216335605b271a3cdd63fa137a69fdf63
Jihwan1801 <116710902+Jihwan1801@users.noreply.github.com>
2022-10-26 16:22:24 +0900

    main.blocks, main.ts 업데이트하기

--- main.ts
+++ main.ts
@@ -3,7 +3,7 @@ music.startMelody(music.builtInMelody(Melodies.Dadadadum), MelodyOptions.Once)
 OLED.init(128, 64)
 basic.forever(function () {
     OLED.clear()
-    OLED.writeString("\"Dust(ug/m3): \"")
+    OLED.writeString("Dust(ug/m3): ")
     미세먼지 = Environment.ReadDust(DigitalPin.P13, AnalogPin.P1)
     OLED.writeNum(미세먼지)
     if (미세먼지 > 30) {
